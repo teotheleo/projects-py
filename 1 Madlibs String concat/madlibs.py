0d2c31cc9ea92d9c3f531c230b48d69f04e3a022
Teotheleo <Teotheleo@email.com>
2022-05-22 11:10:53 +0300

    Initial madlibs game commit

---
+++ 1 Madlibs String concat/madlibs.py
@@ -0,0 +1,20 @@
+# Madlibs using string concatenation
+
+# A paragraph with blank spaces which are going to be filled by the player 
+
+# string concatenation
+#youtuber = "name"
+#print("subscribe to " + youtuber)
+#print("subscribe to {}".format(youtuber))
+#print(f"subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlibs = f"Computer programming is so {adj}! IT makes me so excited all the time because \
+I love to {verb1}. Stay focused and {verb2} like you are {famous_person}"
+
+print(madlibs)
+
